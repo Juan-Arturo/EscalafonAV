fada863caea59776efbdac488e70401f6b2b08eb
Juan-Arturo <jagpjagp002@gmail.com>
2025-02-21 10:05:24 -0600

    enrutamiento nuevo y division de componentes

--- src/app/main/layout/layout.routes.ts
+++ src/app/main/layout/layout.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router'; // Importación del módulo para defin
 import { CoreAuthGuard } from '../../core/guards/core.auth.guard';
 import { LayoutComponent } from './layout.component';
 import { HomeComponent } from './pages/home/home.component';
+import { NewslatterComponent } from './pages/newslatter/newslatter.component';
+import { QuickAcessComponent } from './pages/quick-acess/quick-acess.component';
+import { TeammatesComponent } from './pages/teammates/teammates.component';
+import { ContactComponent } from '../../shared/contact/contact.component';
+import { AboutComponent } from './pages/about/about.component';
 
 
 
@@ -13,6 +18,11 @@ export const layoutRoutes: Routes = [
     // canActivate: [CoreAuthGuard], // El guard AuthGuard asegura que solo los usuarios autenticados pueden acceder a estas rutas
     children: [
         { path: "home", component: HomeComponent },
+        { path: "about", component: AboutComponent },
+        { path: "newslatter", component: NewslatterComponent },
+        { path: "access", component: QuickAcessComponent },
+        { path: "teammates", component: TeammatesComponent },
+        {path: "contact", component: ContactComponent},
 
     ],
   },
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -11,19 +11,23 @@
 
 // }
 
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { ScrollService } from '../../core/services/scroll.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
  constructor(private scrollService: ScrollService) {}
+  ngOnInit(): void {
+    this.goToSection("home")
+  }
+
 
   goToSection(sectionId: string) {
     this.scrollService.scrollToSection(sectionId, '/escalafon');
